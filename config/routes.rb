bc7fd5ec48e0ffd8af254aff5439407204c69e95
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-02-23 12:00:04 +0000

    rutas para pag estaticas v1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
+#  match '/', to: 'static_pages#index', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

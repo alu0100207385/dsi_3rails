7e84866d8065f2c649fce256a5719a5e79cc54fa
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-02-25 10:30:39 +0000

    pruebas HK

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'static_pages#index'
+  #root to: 'static_pages#index'
 #   root 'welcome#index'
 #  match '/', to: 'static_pages#index', via: 'get'
 
